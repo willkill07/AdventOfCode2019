f2d1828429a4d99172e9369a218ba83dbff4683a
Will Killian <william.killian@outlook.com>
2019-12-01 16:47:17 -0500

    [Day01] Finding a reason to range :)

--- src/Day01.cpp
+++ src/Day01.cpp
@@ -1,12 +1,15 @@
 #include "Solution.hpp"
 
 #include <range/v3/all.hpp>
+namespace views = ranges::views;
 
-int fuel_for (int amount) {
+int
+fuel_for(int amount) {
   return amount / 3 - 2;
 }
 
-int total_fuel_for (int amount) {
+int
+total_fuel_for(int amount) {
   amount = fuel_for(amount);
   return (amount <= 0) ? 0 : amount + total_fuel_for(amount);
 }
@@ -15,11 +18,12 @@ template <>
 template <bool part2>
 void
 Day<1>::solve(std::istream &is, std::ostream &os) {
-
-  int(*fn)(int) = (part2) ? total_fuel_for : fuel_for;
-  const std::vector<int> nums{std::istream_iterator<int>{is}, {}};
-  os << ranges::accumulate(nums | ranges::view::transform(fn), 0) << '\n';
+  auto nums = ranges::istream_view<int>(is);
+  auto fun = part2 ? total_fuel_for : fuel_for;
+  os << ranges::accumulate(nums | views::transform(fun), 0) << '\n';
 }
 
-template void Day<1>::solve<true>(std::istream&, std::ostream&);
-template void Day<1>::solve<false>(std::istream&, std::ostream&);
+template void
+Day<1>::solve<true>(std::istream &, std::ostream &);
+template void
+Day<1>::solve<false>(std::istream &, std::ostream &);
